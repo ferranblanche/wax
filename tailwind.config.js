3c7b8aa6462da1ddb34b42e24d4209f12ca56b70
Ferran Blanché <ferran.blanche@gmail.com>
2022-09-08 13:03:31 +0200

    feat: :sparkles: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
+  content: [],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
